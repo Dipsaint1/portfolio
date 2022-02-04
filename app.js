41e78f64848f363d7a0b0f707b5df7a54d4ab679
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-02-04 22:05:39 +0100

    working on the 'more about me section'

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ document.querySelector(".close").addEventListener('click', closeMoreBtn);
 function showMore(){
   // document.querySelector(".more-about-me").style.display = "flex"; 
   // alert(5);
-  document.querySelector(".more-about-me").classList.toggle("on");
-  document.querySelector(".about-container").classList.toggle("off");
+  // document.querySelector(".more-about-me").classList.toggle("on");
+  // document.querySelector(".about-container").classList.toggle("off");
+  document.querySelector(".more-about-me").style.display = "flex";
+  document.querySelector(".about-container").style.display = "none";
 }
 
 function closeMoreBtn(){
   
-  document.querySelector(".more-about-me").classList.toggle("off");
+  document.querySelector(".more-about-me").style.display = "none";
+  document.querySelector(".about-container").style.display = "flex";
   // document.querySelector(".about-container").classList.toggle("on");
 }
