1fd0487639c9c9e3bbf4cb0e2ab5353df024aec3
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-02-04 22:00:38 +0100

    Added javascript to the 'more about me section'

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-document.querySelector(".cv").addEventListener('click', downloadCV);
+document.querySelector(".more-btn").addEventListener('click', showMore);
+document.querySelector(".close").addEventListener('click', closeMoreBtn);
 
 
-downloadCV(() => {
+function showMore(){
+  // document.querySelector(".more-about-me").style.display = "flex"; 
+  // alert(5);
+  document.querySelector(".more-about-me").classList.toggle("on");
+  document.querySelector(".about-container").classList.toggle("off");
+}
 
- });
-
+function closeMoreBtn(){
+  
+  document.querySelector(".more-about-me").classList.toggle("off");
+  // document.querySelector(".about-container").classList.toggle("on");
+}
